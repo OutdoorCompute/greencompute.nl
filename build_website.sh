3916cebad2912f32e10611a3fd7676cc17b02f0f
nick <nick@outdoorcompute.nl>
2024-07-15 21:08:56 +0200

    aoeu

---
+++ build_website.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Building Webpages:"
+
+echo -e 'index\nproducts\nsoftware\ncloud-integration\nabout\ncontact\norder\njourney\ntechnical-specifications\nwelcome\ndatacenters\nhowto\nblog' | while read PAGE
+do
+  echo "  $PAGE-content.html > docs/$PAGE.html"
+
+  cat docs/header.html > docs/$PAGE.html
+  cat docs/$PAGE-content.html >> docs/$PAGE.html
+  cat docs/footer.html >> docs/$PAGE.html
+done
